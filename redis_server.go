4bbf01b822e5dcce0321eeb9aee12e4b10be12ae
Arthur Stockler <admin.sistemas@qg2.com.br>
2017-04-26 23:41:47 -0300

    testando

--- redis_server.go
+++ redis_server.go
@@ -10,7 +10,7 @@ import (
 func startRedis() {
 
 	orderClient.redisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "order-sse-redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
